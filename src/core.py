3391386493fe407d55ac4968b27c59a3c1474887
SergTsch87 <sergtsch87@gmail.com>
2026-04-03 12:05:08 +0300

    Pass test. Upd multiply_str() in core.py

--- src/core.py
+++ src/core.py
@@ -4,11 +4,11 @@
 # no copilot
 
 def multiply_str(str_num_1: str, str_num_2: str) -> str:
-    return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
-    # if (len(str_num_1) == 1 and len(str_num_2) == 1):
-    #     return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
-    # else:
-    #     return str(int(str_num_1) * int(str_num_2))
+    # return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
+    if (len(str_num_1) == 1 and len(str_num_2) == 1):
+        return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
+    else:
+        return str(int(str_num_1) * int(str_num_2))
 
 
 def main():
