8c5fa37cae050cc8bb15ec366a130d43725f4e8b
SergTsch87 <sergtsch87@gmail.com>
2026-04-03 11:59:14 +0300

    Fail test. Add test_multi_by_digit() in test_nums.py

--- src/core.py
+++ src/core.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def multiply_str(str_num_1, str_num_2):
-    return str(int(str_num_1) * int(str_num_2))
+# no copilot
+
+def multiply_str(str_num_1: str, str_num_2: str) -> str:
+    return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
+    # if (len(str_num_1) == 1 and len(str_num_2) == 1):
+    #     return str((ord(str_num_1) - 48) * (ord(str_num_2) - 48))
+    # else:
+    #     return str(int(str_num_1) * int(str_num_2))
 
 
 def main():
--- tests/test_nums.py
+++ tests/test_nums.py
@@ -1,5 +1,10 @@
 import pytest
 from ..src.core import multiply_str
 
+
 def test_single_digit():
     assert multiply_str("3", "4") == "12"
+
+
+def test_multi_by_digit():
+    assert multiply_str("123", "3") == "369"
